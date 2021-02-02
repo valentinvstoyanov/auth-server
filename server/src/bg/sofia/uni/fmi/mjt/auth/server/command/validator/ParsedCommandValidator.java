cf8222f3139f3d9842b39f90124a7bd3ca4831e4
valentinvstoyanov <valentinstoqnov98@gmail.com>
2021-02-02 15:43:06 +0200

    Refactore command validation

--- server/src/bg/sofia/uni/fmi/mjt/auth/server/command/validator/ParsedCommandValidator.java
+++ server/src/bg/sofia/uni/fmi/mjt/auth/server/command/validator/ParsedCommandValidator.java
@@ -8,39 +8,72 @@ import java.util.Set;
 
 public class ParsedCommandValidator implements CommandValidator {
 
-    public static final String COMMAND_EXPECTS_NO_ARGS_FORMAT = "%s command expects no arguments.";
+    public static final String EXPECTS_NO_ARGS_FORMAT = "%s command expects no arguments.";
+    public static final String UNKNOWN_COMMAND_FORMAT = "Unknown command: %s.";
+    public static final String REQUIRED_ARGS_FORMAT = "Command requires %s arguments.";
+    public static final String ARGS_DELIM = ", ";
+    public static final String UNKNOWN_ARGUMETS_FORMAT = "Unknown argumets: %s.";
 
-    //TODO: check whether to split this function into multiple smaller ones
-    //TODO: provide proper exception messages
     public void validate(final ParsedCommand parsedCommand, final Command command) throws InvalidCommandException {
         if (command == null) {
-            throw new InvalidCommandException("1");
+            throw new InvalidCommandException(String.format(UNKNOWN_COMMAND_FORMAT, parsedCommand.name()));
         }
 
-        if (parsedCommand.args() == null) {
+        final Set<String> parsedArgs = parsedCommand.args() == null ? null : parsedCommand.args().keySet();
+        if (parsedArgs == null) {
             if (command.hasRequiredArgs()) {
-                throw new InvalidCommandException("2");
-            } else {
+                final String requiredArgsMessage =
+                        String.format(REQUIRED_ARGS_FORMAT, joinArgs(command.requiredArgs()));
+                throw new InvalidCommandException(requiredArgsMessage);
+            }
+            return;
+        }
+
+        if (command.hasRequiredArgs()) {
+            final Set<String> parsedMinusRequiredArgs = checkMissingArgs(parsedArgs, command.requiredArgs());
+            if (command.hasOptionalArgs()) {
+                checkUnknownArgs(parsedMinusRequiredArgs, command.optionalArgs());
                 return;
             }
+            checkUnknownArgs(parsedMinusRequiredArgs, command.requiredArgs());
+            return;
         }
 
-        final Set<String> parsedArgs = parsedCommand.args().keySet();
-        if (command.hasRequiredArgs()) {
-            if (command.hasOptionalArgs()) {
-                final Set<String> parsedOptionalArgs = new HashSet<>(parsedArgs);
-                parsedOptionalArgs.removeAll(command.requiredArgs());
-                if (!(parsedArgs.containsAll(command.requiredArgs()) && command.optionalArgs().containsAll(parsedOptionalArgs))) {
-                    throw new InvalidCommandException("3");
-                }
-            } else if (!parsedArgs.equals(command.requiredArgs())){
-                throw new InvalidCommandException("4");
-            }
-        } else if (command.hasOptionalArgs() && !command.optionalArgs().containsAll(parsedArgs)) {
-            throw new InvalidCommandException("5");
-        } else {
-            throw new InvalidCommandException(String.format(COMMAND_EXPECTS_NO_ARGS_FORMAT, command.name()));
+        if (command.hasOptionalArgs()) {
+            checkUnknownArgs(parsedArgs, command.optionalArgs());
+            return;
         }
+
+        throw new InvalidCommandException(String.format(EXPECTS_NO_ARGS_FORMAT, command.name()));
+    }
+
+    private Set<String> checkMissingArgs(final Set<String> parsedArgs,
+                                         final Set<String> requiredArgs) throws InvalidCommandException {
+        final Set<String> parsedMinusRequiredArgs = subtractArgs(parsedArgs, requiredArgs);
+        if (parsedMinusRequiredArgs.size() + requiredArgs.size() != parsedArgs.size()) {
+            final Set<String> missingRequiredArgs = subtractArgs(requiredArgs, parsedArgs);
+            final String message = String.format(REQUIRED_ARGS_FORMAT, joinArgs(missingRequiredArgs));
+            throw new InvalidCommandException(message);
+        }
+        return parsedMinusRequiredArgs;
+    }
+
+    private void checkUnknownArgs(final Set<String> parsedArgs,
+                                  final Set<String> expectedArgs) throws InvalidCommandException {
+        final Set<String> unknownArgs = subtractArgs(parsedArgs, expectedArgs);
+        if (unknownArgs.size() > 0) {
+            throw new InvalidCommandException(String.format(UNKNOWN_ARGUMETS_FORMAT, joinArgs(unknownArgs)));
+        }
+    }
+
+    private Set<String> subtractArgs(final Set<String> a, final Set<String> b) {
+        Set<String> result = new HashSet<>(a);
+        result.removeIf(b::contains);
+        return result;
+    }
+
+    private String joinArgs(final Set<String> args) {
+        return String.join(ARGS_DELIM, args);
     }
 
 }
